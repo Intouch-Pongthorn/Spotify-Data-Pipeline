a1cb3f28626b4e7cbdde7e48371943734eee0b4f
Pongthorn Chumpoo <chumpoo.dev@gmail.com>
2023-04-05 23:03:34 +0700

    added artists column

--- src/format.py
+++ src/format.py
@@ -1,13 +1,20 @@
 import datetime
 
-def to_minute(millisecond):
-    second = millisecond //1000
+def to_minute(millisecond:int)->float:
+    second:int = millisecond //1000
     min,sec = divmod(second,60)
-    minute =float(str(f"{min}.{sec}"))
+    minute:float =float(str(f"{min}.{sec}"))
     return minute
+
+def concat(lst:list[str])->str:
+    line:str = ",".join(lst)
+    return line
 
 
 if __name__=="__main__":
     minute = to_minute(228623)
     print(minute)
     print(type(minute))
+    line = concat(["pop","blue"])
+    print(line)
+    
--- src/main.py
+++ src/main.py
@@ -9,16 +9,26 @@ load_dotenv()
 
 file_location = os.getenv('FILE_LOCATION')
 
+def extract_artists_name(lst:list[dict])->str:
+    names:list = []
+    for i in lst:
+        names.append(i["name"])
+    names_one_line = format.concat(names)
+    return names_one_line
+
 history_playlist = get_recently_played(token,50)
 df = pd.DataFrame(
     {
-    "song_name": pd.Series([data["track"]["album"]["name"] for data in history_playlist]),
-    "song_id": pd.Series([data["track"]["album"]["id"] for data in history_playlist]),
+    "song_name": pd.Series([data["track"]["name"] for data in history_playlist]),
     "album_type": pd.Series([data["track"]["album"]["album_type"] for data in history_playlist]),
+    "artist":pd.Series([extract_artists_name(data["track"]["artists"]) for data in history_playlist]),
     "release_date": pd.Series([data["track"]["album"]["release_date"] for data in history_playlist]),
-    "duration_ms": pd.Series([format.to_minute(data["track"]["duration_ms"]) for data in history_playlist]),
+    "duration_minute": pd.Series([format.to_minute(data["track"]["duration_ms"]) for data in history_playlist]),
     "played_at": pd.Series([data["played_at"] for data in history_playlist])
     }
 )
 df.to_csv(file_location+"history.csv")
 print(df)
+
+# for data in history_playlist:
+#     for i  in 
