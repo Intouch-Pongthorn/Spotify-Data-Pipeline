f0f28cb8b2c86c70cde0a37467d679aad619161d
Pongthorn Chumpoo <chumpoo.dev@gmail.com>
2023-04-06 19:16:00 +0700

    added function to convert utc datetime to thai datetime and adjusted some variable names

--- src/format.py
+++ src/format.py
@@ -1,20 +1,36 @@
-import datetime
-
-def to_minute(millisecond:int)->float:
-    second:int = millisecond //1000
-    min,sec = divmod(second,60)
-    minute:float =float(str(f"{min}.{sec}"))
-    return minute
+from datetime import datetime
+import dateutil.parser as dateparser
+import pytz
 
 def concat(lst:list[str])->str:
     line:str = ",".join(lst)
     return line
 
+def extract_artists_name(lst:list[dict])->str:
+    names:list = []
+    for obj in lst:
+        names.append(obj["name"])
+    names_one_line = concat(names)
+    return names_one_line
+
+def to_duration_minute(millisecond:int)->float:
+    second:int = millisecond //1000
+    min,sec = divmod(second,60)
+    duration:float =float(str(f"{min}.{sec}"))
+    return duration
+
+def to_th_time(utc_time_str:str)->datetime:
+    utc_time:datetime = dateparser.parse(utc_time_str)
+    th_time:str = utc_time.astimezone(pytz.timezone('Asia/Bangkok')).strftime('%d-%m-%Y %H:%M:%S')
+    return th_time
+
 
 if __name__=="__main__":
-    minute = to_minute(228623)
+    minute = to_duration_minute(228623)
     print(minute)
     print(type(minute))
     line = concat(["pop","blue"])
     print(line)
+    local_time = to_th_time("2023-04-06T01:30:31.659Z")
+    print(local_time)
     
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from authorize import user_token as token
+from authorize import user_token
 from get import get_recently_played
 import pandas as pd
 from dotenv import load_dotenv
@@ -9,26 +9,16 @@ load_dotenv()
 
 file_location = os.getenv('FILE_LOCATION')
 
-def extract_artists_name(lst:list[dict])->str:
-    names:list = []
-    for i in lst:
-        names.append(i["name"])
-    names_one_line = format.concat(names)
-    return names_one_line
-
-history_playlist = get_recently_played(token,50)
+history_playlist = get_recently_played(user_token,50)
 df = pd.DataFrame(
     {
     "song_name": pd.Series([data["track"]["name"] for data in history_playlist]),
     "album_type": pd.Series([data["track"]["album"]["album_type"] for data in history_playlist]),
-    "artist":pd.Series([extract_artists_name(data["track"]["artists"]) for data in history_playlist]),
+    "artist":pd.Series([format.extract_artists_name(data["track"]["artists"]) for data in history_playlist]),
     "release_date": pd.Series([data["track"]["album"]["release_date"] for data in history_playlist]),
-    "duration_minute": pd.Series([format.to_minute(data["track"]["duration_ms"]) for data in history_playlist]),
-    "played_at": pd.Series([data["played_at"] for data in history_playlist])
+    "duration_minute": pd.Series([format.to_duration_minute(data["track"]["duration_ms"]) for data in history_playlist]),
+    "played_at": pd.Series([format.to_th_time(data["played_at"]) for data in history_playlist])
     }
 )
 df.to_csv(file_location+"history.csv")
 print(df)
-
-# for data in history_playlist:
-#     for i  in 
