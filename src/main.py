6c158aa3c38a6e7c14c9068c2cc087507be85d60
Pongthorn Chumpoo <chumpoo.dev@gmail.com>
2023-05-02 20:45:39 +0700

    put the code into a function

--- src/main.py
+++ src/main.py
@@ -8,19 +8,25 @@ import os
 from datetime import datetime,timedelta
 
 load_dotenv()
-default_dir_location = os.getenv('DEFAULT_DIR_LOCATION') or os.getcwd()
-previous_date = datetime.today() - timedelta(days=1)
-millisec_timestamp = int(previous_date.timestamp() *1000)
-history_playlist = get_recently_played(user_token,after=millisec_timestamp,limit=50)
-df = pd.DataFrame(
-    {
-    "song_name": pd.Series([data["track"]["name"] for data in history_playlist]),
-    "album_type": pd.Series([data["track"]["album"]["album_type"] for data in history_playlist]),
-    "artist":pd.Series([format.extract_artists_name(data["track"]["artists"]) for data in history_playlist]),
-    "release_date": pd.Series([data["track"]["album"]["release_date"] for data in history_playlist]),
-    "duration_minute": pd.Series([format.to_duration_minute(data["track"]["duration_ms"]) for data in history_playlist]),
-    "played_at": pd.to_datetime(pd.Series([format.to_th_time(data["played_at"]) for data in history_playlist])),
-    }
-)
-df.to_csv(default_dir_location+format.set_file_name(previous_date))
-print(df)
+
+def run_spotify_etl(): 
+    default_dir_location = os.getenv('DEFAULT_DIR_LOCATION') or os.getcwd()
+    previous_date = datetime.today() - timedelta(days=1)
+    millisec_timestamp = int(previous_date.timestamp() *1000)
+    history_playlist = get_recently_played(user_token,after=millisec_timestamp,limit=50)
+    df = pd.DataFrame(
+        {
+        "song_name": pd.Series([data["track"]["name"] for data in history_playlist]),
+        "album_type": pd.Series([data["track"]["album"]["album_type"] for data in history_playlist]),
+        "artist":pd.Series([format.extract_artists_name(data["track"]["artists"]) for data in history_playlist]),
+        "release_date": pd.Series([data["track"]["album"]["release_date"] for data in history_playlist]),
+        "duration_minute": pd.Series([format.to_duration_minute(data["track"]["duration_ms"]) for data in history_playlist]),
+        "played_at": pd.to_datetime(pd.Series([format.to_th_time(data["played_at"]) for data in history_playlist])),
+        }
+    )
+    df.to_csv(default_dir_location+format.set_file_name(previous_date))
+    print(df)
+
+
+if __name__ =="__main__":
+    run_spotify_etl()
